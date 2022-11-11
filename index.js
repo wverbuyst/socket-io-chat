fd59ec863899cbf7f520402625a056327307dd4f
Willem Verbuyst <wverbuyst@meditools.nl>
2022-11-11 06:11:17 +0100

    chore: add formatter

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const express = require("express");
-const app = express();
-const path = require("path");
+const express = require('express')
+const app = express()
+const path = require('path')
 
-const http = require("http");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')))
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
-  io.emit("user connected", "user joined");
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  io.emit('user connected', 'user joined')
 
-  socket.on("disconnect", () => {
-    console.log("a user disconnected");
-  });
+  socket.on('disconnect', () => {
+    console.log('a user disconnected')
+  })
 
-  socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
-    io.emit("chat message", msg);
-  });
-});
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg)
+    io.emit('chat message', msg)
+  })
+})
 
 server.listen(3000, () => {
-  console.log("listening on *:3000");
-});
+  console.log('listening on *:3000')
+})
--- public/script.js
+++ public/script.js
@@ -1,26 +1,26 @@
-var socket = io();
+var socket = io()
 
-var info = document.getElementById("info");
-var message = document.getElementById("messages");
-var form = document.getElementById("form");
-var input = document.getElementById("input");
+var info = document.getElementById('info')
+var message = document.getElementById('messages')
+var form = document.getElementById('form')
+var input = document.getElementById('input')
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
+form.addEventListener('submit', function (e) {
+  e.preventDefault()
   if (input.value) {
-    socket.emit("chat message", input.value);
-    input.value = "";
+    socket.emit('chat message', input.value)
+    input.value = ''
   }
-});
+})
 
-socket.on("chat message", function (msg) {
-  var item = document.createElement("li");
-  item.textContent = msg;
-  messages.appendChild(item);
-  window.scrollTo(0, document.body.scrollHeight);
-});
+socket.on('chat message', function (msg) {
+  var item = document.createElement('li')
+  item.textContent = msg
+  messages.appendChild(item)
+  window.scrollTo(0, document.body.scrollHeight)
+})
 
-socket.on("user connected", function (msg) {
-  info.textContent = msg;
-  setTimeout(() => (info.textContent = ""), 2000);
-});
+socket.on('user connected', function (msg) {
+  info.textContent = msg
+  setTimeout(() => (info.textContent = ''), 2000)
+})
